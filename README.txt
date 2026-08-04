510512e738019c2e3d13b3e5f9adf7ac46bdb1e6
Sam <jequibits@gmail.com>
2026-08-04 08:40:06 -0300

    Sprint 0 - Base do projeto e movimentação do jogador

---
+++ README.txt
@@ -0,0 +1,11 @@
+# Dungeon Hunter
+
+Top Down Shooter desenvolvido em Godot 4.5
+
+Status:
+
+✔ Sprint 0 concluída
+
+Versão atual:
+
+0.1.0
---
+++ jogo/scripts/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+func _physics_process(delta: float) -> void:
+	
+	var direction_on_x := Input.get_axis("ui_left", "ui_right")
+	if direction_on_x:
+		velocity.x = direction_on_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var direction_on_y := Input.get_axis("ui_up", "ui_down")
+	if direction_on_y:
+		velocity.y = direction_on_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	move_and_slide()
