ca53b6d4872ea49002601723630e6ed789d051fc
Sam <jequibits@gmail.com>
2026-08-05 13:36:46 -0300

    feat(enemy): add basic Slime chase AI

--- jogo/scripts/controllers/enemy_brain.gd
+++ jogo/scripts/controllers/enemy_brain.gd
@@ -1,6 +1,18 @@
 class_name EnemyBrain extends Node
 
+@export var stop_distance := 20.0
+@export var target: Entity
+
 var entity: Entity
 
 func initialize(owner: Entity):
 	entity = owner
+
+func update():
+	if target == null:
+		return
+	var distance = entity.global_position.distance_to(target.global_position)
+	if distance <= stop_distance:
+		entity.movement.direction = Vector2.ZERO
+	else:
+		entity.movement.direction = entity.global_position.direction_to(target.global_position)
--- jogo/scripts/entities/slime.gd
+++ jogo/scripts/entities/slime.gd
@@ -1,1 +1,10 @@
 class_name Slime extends Entity
+
+func _ready() -> void:
+	super()
+	movement.speed = 70
+
+func _physics_process(delta):
+	brain.update()
+	velocity = movement.get_velocity()
+	move_and_slide()
