b5a94e052bf6a0cbb480007538205509bef5bdc2
Sam <jequibits@gmail.com>
2026-08-06 13:28:54 -0300

    feat(world): create first playable test map

--- jogo/scripts/world.gd
+++ jogo/scripts/world.gd
@@ -1,3 +1,11 @@
 extends Node2D
 
-@onready var player : Player = $TileMapLayer/Player
+@onready var player: Player = $Entities/Player
+@onready var player_spawn: Marker2D = $SpawnPoints/PlayerSpawn
+
+@onready var slime: Slime = $Entities/Slime
+@onready var slime_spawn: Marker2D = $SpawnPoints/SlimeSpawn
+
+func _ready() -> void:
+	player.global_position = player_spawn.global_position
+	slime.global_position = slime_spawn.global_position
