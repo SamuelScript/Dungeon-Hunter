00a1c5501ceb70ab3894355974faff89a2836880
Sam <jequibits@gmail.com>
2026-08-08 08:02:48 -0300

    feat(gameplay): add enemy wave spawning

--- jogo/scripts/systems/enemy_spawner.gd
+++ jogo/scripts/systems/enemy_spawner.gd
@@ -4,22 +4,33 @@ const SLIME_SCENE = preload("res://scenes/entities/Slime.tscn")
 
 @onready var entities: Node2D = $"../Entities"
 @onready var spawn_timer: Timer = $SpawnTimer
-@onready var spawn_point: Marker2D = $SpawnPoint
+@onready var spawn_points := $SpawnPoints.get_children()
 
+@export var max_enemies: int = 3
+@export var spawn_interval: float = 2.0
+
+var alive_enemies: int = 0
 var current_enemy: Slime = null
 
 func _ready():
+	spawn_timer.wait_time = spawn_interval
 	spawn_timer.timeout.connect(_on_spawn_timer_timeout)
-	spawn_enemy()
+	for i in range(max_enemies):
+		spawn_enemy()
 
 func spawn_enemy():
+	if alive_enemies >= max_enemies:
+		return
 	current_enemy = SLIME_SCENE.instantiate()
 	entities.add_child(current_enemy)
-	current_enemy.global_position = spawn_point.global_position
+	alive_enemies += 1
+	var point = spawn_points.pick_random()
+	current_enemy.global_position = point.global_position
 	current_enemy.tree_exited.connect(_on_enemy_died)
 	current_enemy.brain.target = get_tree().get_first_node_in_group("player")
 
 func _on_enemy_died():
+	alive_enemies -= 1
 	$"../GameManager".enemy_killed()
 	current_enemy = null
 	spawn_timer.start()
